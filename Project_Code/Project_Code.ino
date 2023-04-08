03017e43a8b77dec93f44380fe213d72fc4908cd
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-04-08 16:57:34 +0200

    Create Project_Code.ino

---
+++ Project_Code/Project_Code.ino
@@ -0,0 +1,15 @@
+/**
+ * A Rocket-Base simulation, by LEDs instead of Rockets, that can rotates 360 degrees
+ * around X-Axis and 77 degrees around Y-Axis. There're button, Joystick, Servo-Motor,
+ * DC-Motor and 16 LEDs (8 Red LEDs & 8 Yellow LEDs).
+ * 
+ * Operation:-
+ * By moving the joystick up and down, the rockets goes up and down, respectively.
+ * By moving the joystick right and left, the Base rotates right and left, respectively.
+ * By pressing on the joystick button, a yellow LED will emit light that indicates that the
+ * corrosponding rocket is ready to be fired.
+ * By pressing on the seperate button, a red LED will emit light 
+ * -only when corrosponding yellow LED emits- that indicates that the corrosponding rocket is fired.
+ * 
+ * @author Mohamed Sherif
+ */
